2c7cae638652e7849e127d2344929926fa118579
wangchendong <appoleo@163.com>
2021-03-25 15:10:44 +0800

    将给出的链表中的节点每\ k k 个一组翻转，返回翻转后的链表

--- src/main/java/com/ami/study/linkedlist/BaseLogic.java
+++ src/main/java/com/ami/study/linkedlist/BaseLogic.java
@@ -156,6 +156,62 @@ public class BaseLogic {
         return new Node(4, node2);
     }
 
+    /**
+     * 将给出的链表中的节点每\ k k 个一组翻转，返回翻转后的链表
+     * 如果链表中的节点数不是\ k k 的倍数，将最后剩下的节点保持原样
+     * 你不能更改节点中的值，只能更改节点本身。
+     * 要求空间复杂度 \ O(1) O(1)
+     * 例如：
+     * 给定的链表是1\to2\to3\to4\to51→2→3→4→5
+     * 对于 \ k = 2 k=2, 你应该返回 2\to 1\to 4\to 3\to 52→1→4→3→5
+     * 对于 \ k = 3 k=3, 你应该返回 3\to2 \to1 \to 4\to 53→2→1→4→5
+     *
+     * @param head ListNode类
+     * @param k    int整型
+     * @return Node类
+     */
+    public static Node reverseKGroup(Node head, int k) {
+        if (head == null || head.next == null || k <= 1) return head;
+        int count = 0;
+        Node newHead = head;
+        Node end = head;
+        Node prev = null;
+        Node nodeK = null;
+        while (head != null) {
+            count++;
+            if (count % k == 1) {
+                if (!enough(head, k)) {
+                    end.next = head;
+                    break;
+                }
+                prev = null;
+            }
+            if (count == k) newHead = head;
+            if (count % k == 0) {
+                if (nodeK != null) {
+                    end.next = head;
+                    end = nodeK;
+                }
+                nodeK = head.next;
+            }
+            Node tmp = head.next;
+            head.next = prev;
+            prev = head;
+            head = tmp;
+        }
+        return newHead;
+    }
+
+    private static boolean enough(Node head, int k) {
+        Node poi = head;
+        while (poi != null) {
+            k--;
+            if (k <= 0) return true;
+            poi = poi.next;
+        }
+        return false;
+    }
+
     private static void print(Node head) {
         if (head == null) return;
         Node pos = head;
