8ee36eaa125d4df89eafc5a696eae0c8352b4eef
wangchendong <appoleo@163.com>
2021-08-09 18:22:16 +0800

    修改监听器

--- src/main/java/com/ami/study/observable/ApplicationContextListener.java
+++ src/main/java/com/ami/study/observable/ApplicationContextListener.java
@@ -1,16 +1,16 @@
 package com.ami.study.observable;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ApplicationContextEvent;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.support.RequestHandledEvent;
-import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  * @author wangchendong
  * @date 2021/07/27
  */
 @Component // 注册监听器，此方式需事件在ApplicationContext创建完成后才可注册成功，否则采用spring.factories方式
-public class RequestHandlerListener implements ApplicationListener<RequestHandledEvent> {
+public class ApplicationContextListener implements ApplicationListener<ApplicationContextEvent> {
 
     /**
      * Handle an application event.
@@ -18,12 +18,9 @@ public class RequestHandlerListener implements ApplicationListener<RequestHandle
      * @param event the event to respond to
      */
     @Override
-    public void onApplicationEvent(RequestHandledEvent event) {
+    public void onApplicationEvent(ApplicationContextEvent event) {
         System.out.println(event);
-        Object source = event.getSource();
-        if (source instanceof DispatcherServlet) {
-            DispatcherServlet ds = (DispatcherServlet) source;
+        ApplicationContext applicationContext = event.getApplicationContext();
 
-        }
     }
 }
