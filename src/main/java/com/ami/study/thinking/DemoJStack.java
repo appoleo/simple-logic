ae6be147cf43e025d922ecd0c10cac2d2022c89a
wangchendong <appoleo@163.com>
2021-11-29 17:46:47 +0800

    deadlock

---
+++ src/main/java/com/ami/study/thinking/DemoJStack.java
@@ -0,0 +1,32 @@
+package com.ami.study.thinking;
+
+/**
+ * @author wangchendong
+ */
+public class DemoJStack {
+
+    static class SynAddRunnable implements Runnable {
+        int a, b;
+
+        public SynAddRunnable(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public void run() {
+            synchronized (Integer.valueOf(a)) {
+                synchronized (Integer.valueOf(b)) {
+                    System.out.println(a + b);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 100; i++) {
+            new Thread(new SynAddRunnable(1, 2)).start();
+            new Thread(new SynAddRunnable(2, 1)).start();
+        }
+    }
+}
--- src/main/java/com/ami/study/thinking/DemoTest.java
+++ src/main/java/com/ami/study/thinking/DemoTest.java
@@ -2,19 +2,50 @@ package com.ami.study.thinking;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
 /**
  * @author wangchendong
  */
 @Slf4j
 public class DemoTest {
 
-    public static void main(String[] args) {
-        String str1 = new StringBuilder("计算机").append("软件").toString();
-        // System.out.println(str1);
-        System.out.println(str1.intern() == str1);
+    class GrandFather {
+        void thinking() {
+            System.out.println("i am grandfather");
+        }
+    }
 
-        String str2 = new StringBuilder("ja").append("va").toString();
-        // System.out.println(str2);
-        System.out.println(str2.intern() == str2);
+    class Father extends GrandFather {
+        void thinking() {
+            System.out.println("i am father");
+        }
+    }
+
+    class Son extends Father {
+        void thinking() {
+            // 请读者在这里填入适当的代码（不能修改其他地方的代码）
+            // 实现调用祖父类的thinking()方法，打印"i am grandfather"
+            try {
+                MethodType methodType = MethodType.methodType(void.class);
+                MethodHandle methodHandle = MethodHandles.lookup()
+                        .findSpecial(GrandFather.class, "thinking", methodType, getClass());
+
+                // MethodType methodType = MethodType.methodType(void.class);
+                // Field lookupImpl = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
+                // lookupImpl.setAccessible(true);
+                // MethodHandle methodHandle = ((MethodHandles.Lookup) lookupImpl.get(null))
+                //         .findSpecial(GrandFather.class, "thinking", methodType, GrandFather.class);
+                methodHandle.invoke(this);
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new DemoTest().new Son().thinking();
     }
 }
